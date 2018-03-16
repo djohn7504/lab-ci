89c0a6bbbe3e64dccf8f9a80c2c1cd10ead08676
djohn7504 <37219628+djohn7504@users.noreply.github.com>
2018-03-16 21:04:31 +0100

    Update deploy-gh-pages-03.sh

--- deploy-gh-pages-03.sh
+++ deploy-gh-pages-03.sh
@@ -4,21 +4,18 @@ echo "===================================="
 echo "--------------- starting deploy..."
 echo "===================================="
 
-if [ $TRAVIS ] && [ $TRAVIS_OS_NAME == "linux" ]
-then
+if [ $TRAVIS ] && [ $TRAVIS_OS_NAME = "linux" ]; then
   echo "Hello Travis CI, $TRAVIS_OS_NAME"
   textSearch="Hello Linux"
   textSearch="Hello Linux.*)"
   textReplace="Hello Linux (build $APPVEYOR_BUILD_VERSION)"
 
-elif [ $TRAVIS ] && [ $TRAVIS_OS_NAME == "osx" ]
-then
+elif [ $TRAVIS ] && [ $TRAVIS_OS_NAME = "osx" ]; then
   echo "Hello Travis CI, $TRAVIS_OS_NAME"
   textSearch="Hello macOS"
   textReplace="Hello macOS (build $APPVEYOR_BUILD_VERSION)"
 
-elif [ $APPVEYOR ]
-then
+elif [ $APPVEYOR ]; then
   echo "Hello AppVeyor CI, $PLATFORM"
   textSearch="Hello Windows"
   textReplace="Hello Windows (build $APPVEYOR_BUILD_VERSION)"
