b322eed9e43a0ae5ac8cdb48ea4871f1f89e0060
djohn7504 <37219628+djohn7504@users.noreply.github.com>
2018-03-16 13:35:50 +0100

    Create deploy-gh-pages.sh

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf out || exit 0;
+
+mkdir out;
+
+echo "starting deploy..."
+( cd out
+ git init
+ git config user.name ${GIT_NAME}
+ git config user.email ${GIT_EMAIL}
+# cp -r ../public/* ./
+# cp ../release/myproject.min.js ./scripts/myproject.min.js
+ cp ../deploy/index-linux.html ./index.html
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci" master:gh-pages > /dev/null 2>&1
+)
