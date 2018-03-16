13f388c62602d10e09465f82966da60a29b00872
djohn7504 <37219628+djohn7504@users.noreply.github.com>
2018-03-16 14:30:04 +0100

    Update deploy-gh-pages.sh

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -6,17 +6,16 @@ echo "===================================="
 
 rm -rf out || exit 0;
 
-mkdir out;
+mkdir out
+cd out
 
-echo "starting deploy..."
-( cd out
- git init
- git config user.name ${GIT_NAME}
- git config user.email ${GIT_EMAIL}
-# cp -r ../public/* ./
-# cp ../release/myproject.min.js ./scripts/myproject.min.js
- cp ../deploy/index-linux.html ./index.html
- git add .
- git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci" master:gh-pages > /dev/null 2>&1
-)
+#git init
+#git config user.name ${GIT_NAME}
+#git config user.email ${GIT_EMAIL}
+git checkout "gh-pages"
+
+cp ../deploy/index-linux.html ./index.html
+
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci" master:gh-pages > /dev/null 2>&1
