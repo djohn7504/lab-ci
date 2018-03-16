5171602283ecbf2632f53331903cf5fe1318978b
djohn7504 <37219628+djohn7504@users.noreply.github.com>
2018-03-16 21:18:25 +0100

    Update deploy-gh-pages-03.sh

--- deploy-gh-pages-03.sh
+++ deploy-gh-pages-03.sh
@@ -4,50 +4,62 @@ echo "===================================="
 echo "--------------- starting deploy..."
 echo "===================================="
 
-if [ $TRAVIS ] && [ $TRAVIS_OS_NAME = "linux" ]; then
-  echo "Hello Travis CI, $TRAVIS_OS_NAME"
-  textSearch="Hello Linux"
-  textSearch="Hello Linux.*)"
-  textReplace="Hello Linux (build $TRAVIS_BUILD_NUMBER)"
-
-elif [ $TRAVIS ] && [ $TRAVIS_OS_NAME = "osx" ]; then
-  echo "Hello Travis CI, $TRAVIS_OS_NAME"
-  textSearch="Hello macOS"
-  textReplace="Hello macOS (build $TRAVIS_BUILD_NUMBER)"
-
-elif [ $APPVEYOR ]; then
-  echo "Hello AppVeyor CI, $PLATFORM"
-  textSearch="Hello Windows"
-  textReplace="Hello Windows (build $APPVEYOR_BUILD_VERSION)"
-
-else
-  echo "Hello UNKNOW";
-  exit 0;
-
-fi
-
 rm -rf out || exit 0;
 git clone --branch "gh-pages" https://github.com/djohn7504/lab-ci.git "out"
 cd out
 
 
-#textSearch="\[linux\]"
-#textSearch="Hello Linux"
-#textReplace="Hello Linux (build $TRAVIS_BUILD_NUMBER)"
-sedComportment=""
-targetFile="./index.html"
-
-#	BSD/macOS		sed -i "" "$@"
-#	GNU 			sed -i -- "$@"
-	
-	sed		-i --											\
-			"s/$textSearch/$textReplace/$sedComportment"	\
-			"./$targetFile"					
-			#
+# BSD/macOS	sed -i "" "$@"
+# GNU 	sed -i -- "$@"
 
 
+if [ $TRAVIS ] && [ $TRAVIS_OS_NAME = "linux" ]; then
+  echo "Hello Travis CI, $TRAVIS_OS_NAME"
+  textSearch="Hello Linux"
+  textSearch="Hello Linux.*)"
+  textReplace="Hello Linux (build $TRAVIS_BUILD_NUMBER)"
+  sedComportment=""
+  targetFile="./index.html"
+  sed -i -- "s/$textSearch/$textReplace/$sedComportment" "./$targetFile"
+  git add .
+  git commit -m "Deployed to Github Pages"
+  git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci"
 
 
-git add .
-git commit -m "Deployed to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci"
+
+elif [ $TRAVIS ] && [ $TRAVIS_OS_NAME = "osx" ]; then
+  echo "Hello Travis CI, $TRAVIS_OS_NAME"
+  textSearch="Hello macOS"
+  textReplace="Hello macOS (build $TRAVIS_BUILD_NUMBER)"
+  sedComportment=""
+  targetFile="./index.html"
+# sed -i -- "s/$textSearch/$textReplace/$sedComportment" "./$targetFile"
+  sed -i "" "s/$textSearch/$textReplace/$sedComportment" "./$targetFile"
+  git add .
+  git commit -m "Deployed to Github Pages"
+  git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci"
+
+
+
+elif [ $APPVEYOR ]; then
+  echo "Hello AppVeyor CI, $PLATFORM"
+  textSearch="Hello Windows"
+  textReplace="Hello Windows (build $APPVEYOR_BUILD_VERSION)"
+  sedComportment=""
+  targetFile="./index.html"
+  sed -i -- "s/$textSearch/$textReplace/$sedComportment" "./$targetFile"
+  git add .
+  git commit -m "Deployed to Github Pages"
+  GITHUB_TOKEN=7RRqPIF4QkklZNMCs/INNpMUiiYad2N3zxkzgMc6isuBdd95NuoImqA9fzOvrjHg
+# git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci"
+  git push --force --quiet "https://$GITHUB_TOKEN@github.com/djohn7504/lab-ci"
+
+
+
+else
+  echo "Hello UNKNOW";
+  exit 0;
+
+
+
+fi
