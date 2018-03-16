e621a2159aab9660529e1f704f2ba3fe8efbbfcc
djohn7504 <37219628+djohn7504@users.noreply.github.com>
2018-03-16 18:26:01 +0100

    Update deploy-gh-pages-03.sh

--- deploy-gh-pages-03.sh
+++ deploy-gh-pages-03.sh
@@ -11,6 +11,24 @@ rm -rf out || exit 0;
 
 git clone --branch "gh-pages" https://github.com/djohn7504/lab-ci.git "out"
 cd out
+
+
+textSearch="\[linux\]"
+textReplace="Hello Linux (build $TRAVIS_BUILD_NUMBER)"
+sedComportment=""
+targetFile="./index.html"
+
+#	BSD/macOS		sed -i "" "$@"
+#	GNU 			sed -i -- "$@"
+	
+	sed		-i --											\
+			"s/$textSearch/$textReplace/$sedComportment"	\
+			"./$targetFile"					
+			#
+
+
+
+
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci"
