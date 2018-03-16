3ddc539d331971ff502f42ae091da33c62ab2bff
djohn7504 <37219628+djohn7504@users.noreply.github.com>
2018-03-16 20:48:22 +0100

    Update deploy-gh-pages-03.sh

--- deploy-gh-pages-03.sh
+++ deploy-gh-pages-03.sh
@@ -4,23 +4,39 @@ echo "===================================="
 echo "--------------- starting deploy..."
 echo "===================================="
 
-if ( $TRAVIS )
+if [ $TRAVIS ] && [ $TRAVIS_OS_NAME == "linux" ]
 then
   echo "Hello Travis CI, $TRAVIS_OS_NAME"
+  textSearch="Hello Linux"
+  textSearch="Hello Linux.*)"
+  textReplace="Hello Linux (build $APPVEYOR_BUILD_VERSION)"
+
+if [ $TRAVIS ] && [ $TRAVIS_OS_NAME == "osx" ]
+then
+  echo "Hello Travis CI, $TRAVIS_OS_NAME"
+  textSearch="Hello macOS"
+  textReplace="Hello macOS (build $APPVEYOR_BUILD_VERSION)"
+
+elif ( $APPVEYOR )
+then
+  echo "Hello AppVeyor CI, $PLATFORM"
+  textSearch="Hello Windows"
+  textReplace="Hello Windows (build $APPVEYOR_BUILD_VERSION)"
+
+else
+  echo "Hello UNKNOW";
+  exit 0;
+
 fi
 
 rm -rf out || exit 0;
-
-#mkdir out
-#cd out
-
 git clone --branch "gh-pages" https://github.com/djohn7504/lab-ci.git "out"
 cd out
 
 
-textSearch="\[linux\]"
-textSearch="Hello Linux"
-textReplace="Hello Linux (build $TRAVIS_BUILD_NUMBER)"
+#textSearch="\[linux\]"
+#textSearch="Hello Linux"
+#textReplace="Hello Linux (build $TRAVIS_BUILD_NUMBER)"
 sedComportment=""
 targetFile="./index.html"
 
@@ -38,4 +54,3 @@ targetFile="./index.html"
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci"
-#git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci" master:gh-pages > /dev/null 2>&1
