d97041c6c9751921301465139c6f567519267b37
mojoco <>
2018-03-17 23:41:15 +0100

    ..

--- travis-to-releases.sh
+++ travis-to-releases.sh
@@ -12,9 +12,14 @@ echo "===================================="
 
 
 echo "===================================="
+echo "fetch:"
 # To make it create such a tag if it does not yet exist:
 git fetch origin refs/tags/test-01:refs/tags/test-01
 echo "===================================="
+echo "tag list:"
+git tag --list
+echo "===================================="
+echo "checkout tag:"
 git checkout tags/release-test
 echo "===================================="
 ls $TRAVIS_BUILD_DIR
