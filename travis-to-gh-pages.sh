653d8e4dcb2b67ea8e37d9364b90b6089f5465a4
mojoco <>
2018-03-17 20:55:46 +0100

    ..

--- travis-to-gh-pages.sh
+++ travis-to-gh-pages.sh
@@ -12,17 +12,16 @@ echo "===================================="
 
 
 #if [ $(git ls-remote --exit-code --tags origin release-test) ]; then
-#  echo "OK exist";
-#else
-#  echo "NOT exist !!!";
-#fi
+if git ls-remote --exit-code --tags origin release-test >/dev/null 2>&1 ; then
+  echo "OK exist";
+else
+  echo "NOT exist !!!";
+fi
 
 
 
 
 # To make it create such a tag if it does not yet exist:
-# git fetch origin refs/tags/1.0.0:refs/tags/1.0.0
-git fetch origin refs/tags/release-test:refs/tags/release-test
-
-
-ls $TRAVIS_BUILD_DIR
+#git fetch origin refs/tags/release-test:refs/tags/release-test
+#git status
+#ls $TRAVIS_BUILD_DIR
