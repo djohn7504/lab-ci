3f28e5577ae16faa06dcb730da59a3c680401143
mojoco <>
2018-03-18 21:27:25 +0100

    ..

--- travis-to-gh-pages.sh
+++ travis-to-gh-pages.sh
@@ -6,26 +6,33 @@ set -e
 echo "================================================="
 echo "--------------- starting deploy to GitHub Pages.."
 echo "================================================="
+echo
 
-echo 
 
 echo "================================================="
 echo "--------------- on branch gh-pages"
 echo "================================================="
-
+#		see: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 echo 
+
+
+
+
 
 echo "================================================="
 echo "--------------- on branch master, folder /docs"
 echo "================================================="
 
 
+#git config --global user.name ${GIT_NAME}
+#git config --global user.email ${GIT_EMAIL}
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 git checkout master
-git config user.name ${GIT_NAME}
-git config user.email ${GIT_EMAIL}
 echo "Travis CI (build $TRAVIS_BUILD_VERSION)" >> docs/README.md
 git add .
-git commit -m "Update GitHub Pages index."
+#git commit --message "Update GitHub Pages index."
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 #git push
 #git push origin HEAD:master
 #git push --force --quiet "https://${GITHUB_TOKEN}@github.com/djohn7504/lab-ci" master:gh-pages > /dev/null 2>&1
