2f94a484d33b2f26c4b4f8288a9efdc75ff9df9b
mojoco <>
2018-03-18 23:23:55 +0100

    ..

--- travis-to-gh-pages.sh
+++ travis-to-gh-pages.sh
@@ -12,27 +12,16 @@ echo
 echo "================================================="
 echo "--------------- on branch gh-pages"
 echo "================================================="
-#		see: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 echo 
-#git config --global user.email "travis@travis-ci.org"
-#git config --global user.name "Travis CI"
-#git fetch --all
-#git pull --all
-#git checkout gh-pages
-#echo "Travis CI (build $TRAVIS_BUILD_NUMBER)" >> index.html
-#git add .
-#git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-#git push --force --quiet "https://${GITHUB_TOKEN_2}@github.com/djohn7504/lab-ci" master
-
-
 
 rm -rf out || exit 0;
-
 mkdir out
 cd out
 
 git clone --branch "gh-pages" https://github.com/djohn7504/lab-ci.git .
 echo "<br>Travis CI (build $TRAVIS_BUILD_NUMBER)" >> index.html
+echo "" >> README.md
+echo "####Travis CI (build $TRAVIS_BUILD_NUMBER)" >> README.md
 git add index.html
 git commit --message "Change index.html"
 git push --force --quiet "https://$GITHUB_TOKEN_2@github.com/djohn7504/lab-ci"
