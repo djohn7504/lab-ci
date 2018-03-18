67c3277fdbe855286d4f012b68189488dd2c839c
mojoco <>
2018-03-18 23:27:15 +0100

    ..

--- travis-to-gh-pages.sh
+++ travis-to-gh-pages.sh
@@ -21,7 +21,7 @@ cd out
 git clone --branch "gh-pages" https://github.com/djohn7504/lab-ci.git .
 echo "<br>Travis CI (build $TRAVIS_BUILD_NUMBER)" >> index.html
 echo "" >> README.md
-echo "####Travis CI (build $TRAVIS_BUILD_NUMBER)" >> README.md
+echo "#### Travis CI (build $TRAVIS_BUILD_NUMBER)" >> README.md
 git add index.html README.md
 git commit --message "Change index.html"
 git push --force --quiet "https://$GITHUB_TOKEN_2@github.com/djohn7504/lab-ci"
