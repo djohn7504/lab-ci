4a6d7bcb29ee8ed0a6001a29ef7b83ebdf90ed47
mojoco <>
2018-03-18 22:15:56 +0100

    ..

--- travis-to-gh-pages.sh
+++ travis-to-gh-pages.sh
@@ -14,6 +14,13 @@ echo "--------------- on branch gh-pages"
 echo "================================================="
 #		see: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 echo 
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git checkout gh-pages
+#echo "Travis CI (build $TRAVIS_BUILD_NUMBER)" >> docs/README.md
+#git add .
+#git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+#git push --force --quiet "https://${GITHUB_TOKEN_2}@github.com/djohn7504/lab-ci" master
 
 
 
@@ -23,17 +30,13 @@ echo "================================================="
 echo "--------------- on branch master, folder /docs"
 echo "================================================="
 
+#	OK, but
+#	INFINITE LOOP !!!!!!!!!!!!!!!!
 
-#git config --global user.name ${GIT_NAME}
-#git config --global user.email ${GIT_EMAIL}
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git checkout master
-echo "Travis CI (build $TRAVIS_BUILD_VERSION)" >> docs/README.md
-git add .
-#git commit --message "Update GitHub Pages index."
-git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-#git push
-#git push origin HEAD:master
-#git push --force --quiet "https://${GITHUB_TOKEN_2}@github.com/djohn7504/lab-ci" master:gh-pages > /dev/null 2>&1
-git push --force --quiet "https://${GITHUB_TOKEN_2}@github.com/djohn7504/lab-ci" master
+#git config --global user.email "travis@travis-ci.org"
+#git config --global user.name "Travis CI"
+#git checkout master
+#echo "Travis CI (build $TRAVIS_BUILD_NUMBER)" >> docs/README.md
+#git add .
+#git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+#git push --force --quiet "https://${GITHUB_TOKEN_2}@github.com/djohn7504/lab-ci" master
