bedf523bcfdd987d8380eedd10dbc2f4d3c031da
mojoco <>
2018-03-18 23:39:15 +0100

    ..

--- travis-to-gh-pages.sh
+++ travis-to-gh-pages.sh
@@ -8,37 +8,19 @@ echo "--------------- starting deploy to GitHub Pages.."
 echo "================================================="
 echo
 
-
-echo "================================================="
-echo "--------------- on branch gh-pages"
-echo "================================================="
-echo 
-
+# where..
 rm -rf out || exit 0;
 mkdir out
 cd out
 
+# what..
 git clone --branch "gh-pages" https://github.com/djohn7504/lab-ci.git .
 
+# modify..
 echo "" >> README.md
 echo "#### Travis CI (build $TRAVIS_BUILD_NUMBER)" >> README.md
 git add README.md
-
 git commit --message "Update GitHub Pages."
+
+# send..
 git push --force --quiet "https://$GITHUB_TOKEN_2@github.com/djohn7504/lab-ci"
-
-
-echo "================================================="
-echo "--------------- on branch master, folder /docs"
-echo "================================================="
-
-#	OK, but
-#	INFINITE LOOP !!!!!!!!!!!!!!!!
-
-#git config --global user.email "travis@travis-ci.org"
-#git config --global user.name "Travis CI"
-#git checkout master
-#echo "Travis CI (build $TRAVIS_BUILD_NUMBER)" >> docs/README.md
-#git add .
-#git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-#git push --force --quiet "https://${GITHUB_TOKEN_2}@github.com/djohn7504/lab-ci" master
