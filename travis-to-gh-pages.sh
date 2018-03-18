a902ab6591d20d93a05d06664fce35772f7cdd1c
mojoco <>
2018-03-18 22:17:43 +0100

    ..

--- travis-to-gh-pages.sh
+++ travis-to-gh-pages.sh
@@ -16,6 +16,7 @@ echo "================================================="
 echo 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
+git fetch --all
 git checkout gh-pages
 #echo "Travis CI (build $TRAVIS_BUILD_NUMBER)" >> docs/README.md
 #git add .
