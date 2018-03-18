ccce5ae76a2b7fe46bd350985c13027a95be1d82
mojoco <>
2018-03-18 16:56:48 +0100

    ..

--- travis-to-gh-pages.sh
+++ travis-to-gh-pages.sh
@@ -3,11 +3,10 @@
 # Make sure the script fails when any of the commands fail.
 set -e
 
-TAG_NAME=release-test
-TAG_NAME=gh-pages
+BRANCH_NAME=gh-pages
 
 echo "===================================="
-echo "--------------- starting deploy to $TAG_NAME.."
+echo "--------------- starting deploy to $BRANCH_NAME.."
 echo "--------------- $(uname)"
 # Darwin, Linux, ?..
 echo "===================================="
@@ -15,25 +14,10 @@ echo "===================================="
 
 
 
-if (git ls-remote --exit-code --tags origin $TAG_NAME >/dev/null 2>&1) ; then
-  echo "$TAG_NAME exist on remote";
-else
-  echo "$TAG_NAME NOT exist on remote !!!";
-fi
-
 
 
 echo "===================================="
-git tag --list
-echo "===================================="
-# To make it create such a tag if it does not yet exist:
-#git fetch origin refs/tags/release-test:refs/tags/release-test
-
-echo "===================================="
-#git status
-git tag --list
-
-echo "===================================="
-git checkout tags/release-test
+git branch
+#git checkout $BRANCH_NAME
 echo "===================================="
 ls $TRAVIS_BUILD_DIR
--- travis-to-releases.sh
+++ travis-to-releases.sh
@@ -17,32 +17,20 @@ if [ $(git tag --list "$TAG_NAME" == $TAG_NAME) ] ; then
   echo "-------------";
   git checkout release-test
   git show $TAG_NAME
+  # ..
+
 elif git ls-remote --exit-code --tags origin $TAG_NAME >/dev/null 2>&1 ; then
   echo "use the remote $TAG_NAME:";
   echo "--------------";
+  # need download ?..
+
 else
   echo "$TAG_NAME NOT exist !!!";
-fi
+  # need create ?..
 
-echo "===================================="
-
-if git ls-remote --exit-code --tags origin $TAG_NAME >/dev/null 2>&1 ; then
-  echo "$TAG_NAME exist on remote";
-  #git fetch origin refs/tags/release-test:refs/tags/release-test
-else
-  echo "$TAG_NAME NOT exist on remote !!!";
 fi
 
 
-echo "===================================="
-echo "fetch:"
-# To make it create such a tag if it does not yet exist:
-#git fetch origin refs/tags/test-01:refs/tags/test-01
-echo "===================================="
-echo "tag list:"
-git tag --list
-echo "===================================="
-echo "checkout tag:"
-#git checkout tags/release-test
+
 echo "===================================="
 ls $TRAVIS_BUILD_DIR
