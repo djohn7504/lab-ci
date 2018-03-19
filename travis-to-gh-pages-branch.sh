276399ffb48fe070d8c915789a0bb205bf7666e6
mojoco <>
2018-03-19 23:05:48 +0100

    ..

---
+++ travis-to-gh-pages-branch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Make sure the script fails when any of the commands fail.
+set -e
+
+echo "================================================="
+echo "--------------- starting deploy to GitHub Pages.."
+echo "================================================="
+echo
+
+# where..
+rm -rf out || exit 0;
+mkdir out
+cd out
+
+# what..
+git clone --branch "gh-pages" https://github.com/djohn7504/lab-ci.git .
+
+# modify..
+echo "" >> README.md
+echo "#### Travis CI (build $TRAVIS_BUILD_NUMBER)" >> README.md
+git add README.md
+git commit --message "Update GitHub Pages."
+
+# send..
+git push --force --quiet "https://$GITHUB_TOKEN@github.com/djohn7504/lab-ci"
+
+# cleanup..
+cd ..
