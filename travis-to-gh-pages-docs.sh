7eb47d05a153b2d7358a59801388a15fdb1260a5
mojoco <>
2018-03-20 23:18:23 +0100

    ..

--- travis-to-gh-pages-docs.sh
+++ travis-to-gh-pages-docs.sh
@@ -10,7 +10,7 @@ echo
 
 
 git fetch --unshallow --tags
-#git checkout master
+git checkout master
 
 
 echo "" >> Docs/README.md
@@ -20,6 +20,6 @@ git add Docs/README.md
 # IMPORTANT: [skip ci] to avoid infinite loop !!!
 # *********************************************************************************
 git commit --message "Update GitHub Pages. (Docs folder) [skip ci]"
-#git push --force --quiet "https://$GITHUB_TOKEN@github.com/djohn7504/lab-ci"
+git push --force --quiet "https://$GITHUB_TOKEN@github.com/djohn7504/lab-ci"
 #git push "https://$GITHUB_TOKEN@github.com/djohn7504/lab-ci"
-git push "https://$GITHUB_TOKEN@github.com/djohn7504/lab-ci" HEAD:master
+#git push "https://$GITHUB_TOKEN@github.com/djohn7504/lab-ci" HEAD:master
